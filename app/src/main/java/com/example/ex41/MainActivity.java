8193614b91a57d3905fd71675be13297b61afedb
nzo2020 <nz2020@bs.amalnet.k12.il>
2023-12-15 19:57:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex41/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ex41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    // method that start when button clicked
+    public void clicked(View view) {
+        btn.setText("Oh yeah, I've been clicked!");
+    }
+}
